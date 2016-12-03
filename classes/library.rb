9c7c96bac6905c608163aa009eb8098d8128f321
za4emnik <maxots@rambler.ru>
2016-12-03 20:59:00 +0200

    refactoring: add loader module and refactor methods

--- classes/library.rb
+++ classes/library.rb
@@ -1,59 +1,31 @@
+load 'modules/loader.rb'
+
 class Library
+
+  include Loader
+
   attr_accessor :books, :orders, :readers, :authors
-  attr_writer :data
-  def initialize(books = nil, orders = nil, readers = nil, authors = nil)
-    @books = books
-    @orders = orders
-    @readers = readers
-    @authors = authors
+
+  def initialize()
+    @books, @orders, @readers, @authors = [],[],[],[]
+    fill_random_data if File.zero?('data.yml')
   end
 
   def most_popular_book
-    most_popular('book').title
+    most_popular(:book, :title)
   end
 
   def often_takes_books
-    most_popular('reader').name
+    puts most_popular(:reader, :name)
   end
 
-  def how_order_most_popular
-    @data.orders.uniq(&:reader).map(&:book).group_by(&:title).values.first(3).map(&:length).sum
-  end
-
-  def read_data
-    @data = YAML.load_file('data.yml')
+  def how_order_most_popular(number)
+    popular_book = most_popular(:book, :title, number)
+    @orders.select{|order| popular_book.include?(order.book.title)}.uniq(&:reader).size
   end
 
   private
-  def most_popular(type)
-    @data.orders.map(&type.to_sym).group_by(&:itself).values.max_by { :itself.length }.first
-  end
-
-  def self::fill_random_data
-    readers = []
-    15.times do
-      readers << Reader.new(Faker::Name.name, Faker::Internet.email, Faker::Address.city, Faker::Address.street_name, Faker::Address.building_number)
-    end
-
-      authors = []
-      15.times do
-        authors << Author.new(Faker::Name.name, Faker::Lorem.sentence)
-      end
-
-      books = []
-      15.times do
-        books << Book.new(Faker::Book.title, authors.sample)
-      end
-
-      orders = []
-      15.times do
-        orders << Order.new(books.sample, readers.sample, Faker::Date.backward(14))
-      end
-
-        library = Library.new(books, orders, readers, authors)
-
-        File.open('data.yml', 'w') do |f|
-          f.write library.to_yaml
-        end
+  def most_popular(type, field, number=1)
+    @orders.map(&type).group_by(&:itself).sort{:itself.length}.first(number).flatten.map(&field).uniq
   end
 end
--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'require_all'
 require 'active_support/all'
 require 'yaml'
 require 'faker'
+require 'pp'
 require_all 'classes'
-
-Library.fill_random_data if File.zero?('data.yml')
+require_all 'modules'
 
 library = Library.new
 library.read_data
@@ -14,4 +14,4 @@ puts library.often_takes_books
 p 'Most popular book'
 puts library.most_popular_book
 p 'Peoples ordered one of the three most popular books'
-puts library.how_order_most_popular
+puts library.how_order_most_popular 3
